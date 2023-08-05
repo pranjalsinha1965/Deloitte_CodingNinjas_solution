b71427ac54a51ef1e203879f959fd69570c2e3e0
Pranjal Sinha <87580655+pranjalsinha1965@users.noreply.github.com>
2023-08-05 15:33:37 +0530

    Update Pair Sum.cpp

--- Pair Sum.cpp
+++ Pair Sum.cpp
@@ -1,21 +1,24 @@
 #include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+using namespace std;
 vector<vector<int>> pairSum(vector<int> &a, int s){
    // Write your code here
     vector<vector<int>> ans;
-   int n = a.size();
-    for(int i =0;i<n;i++)
+    int n = a.size();
+    for(int i = 0; i < n; i++)
     {
-        for(int j =i+1;j<n;j++)
+        for(int j = i + 1; j < n; j++)
         {
-          if (a[i] + a[j] == s) {
-            vector<int> temp;
-            temp.push_back(min(a[i], a[j]));
-            temp.push_back(max(a[i], a[j]));
-            ans.push_back(temp);
-          }
+            if (a[i] + a[j] == s) {
+                vector<int> temp;
+                temp.push_back(min(a[i], a[j]));
+                temp.push_back(max(a[i], a[j]));
+                ans.push_back(temp);
+            }
         }
     }
-    sort(ans.begin(),ans.end());
-    return ans ;
+    sort(ans.begin(), ans.end());
+    return ans;
 }
+
