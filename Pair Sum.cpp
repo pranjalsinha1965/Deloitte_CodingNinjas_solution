49decdc0403330a3ee2c5b310cf0a4abac918676
Pranjal Sinha <87580655+pranjalsinha1965@users.noreply.github.com>
2023-08-05 15:47:39 +0530

    Update Pair Sum.cpp

--- Pair Sum.cpp
+++ Pair Sum.cpp
@@ -1,16 +1,15 @@
 #include <bits/stdc++.h>
-#include <iostream>
 #include <vector>
+#include<algorithm>
 using namespace std;
+/* ----------------X SOLUTION PART ----------------X */
 vector<vector<int>> pairSum(vector<int> &a, int s){
-   // Write your code here
     vector<vector<int>> ans;
     int n = a.size();
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = i + 1; j < n; j++)
-        {
-            if (a[i] + a[j] == s) {
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(a[i] + a[j] == s)
+            {
                 vector<int> temp;
                 temp.push_back(min(a[i], a[j]));
                 temp.push_back(max(a[i], a[j]));
@@ -18,7 +17,35 @@ vector<vector<int>> pairSum(vector<int> &a, int s){
             }
         }
     }
+
     sort(ans.begin(), ans.end());
     return ans;
 }
+/* ----------------X SOLUTION PART ----------------X */
+int main() {
+    int n, targetSum;
+    cout << "Enter the size of the array: ";
+    cin >> n;
 
+    vector<int> arr(n);
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter the target sum: ";
+    cin >> targetSum;
+
+    vector<vector<int>> result = pairSum(arr, targetSum);
+
+    if (result.empty()) {
+        cout << "No pairs found that add up to the target sum." << endl;
+    } else {
+        cout << "Pairs found: " << endl;
+        for (vector<int> pair : result) {
+            cout << pair[0] << " " << pair[1] << endl;
+        }
+    }
+
+    return 0;
+}
